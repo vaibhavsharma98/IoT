324a5bccc07b23f9f01dca130dc40486722f121e
Vaibhav Sharma <61656345+vaibhavsharma98@users.noreply.github.com>
2020-05-13 09:43:54 +0530

    Create led_blink_array.ino

---
+++ Embedded C/led_blink_array.ino
@@ -0,0 +1,17 @@
+#include<avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16000000L
+
+int main (void)
+{
+  DDRB=0b00000011;
+int arr[7]= {0x01,0x02,0x04,0x08,0x40,0x20,0x10};
+  while(1)
+  {
+    for (int i=0;i<8;i++)
+    {
+     PORTB= arr[i];
+    _delay_ms(500);
+    }
+  }
+}
