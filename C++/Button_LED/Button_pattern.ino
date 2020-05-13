eae250fa0ac6ae4dceaa432ef5e4477f1ae0383a
vaibhavsharma98 <61656345+vaibhavsharma98@users.noreply.github.com>
2020-05-13 06:56:39 +0530

    Update Button_pattern.ino

--- C++/Button_LED/Button_pattern.ino
+++ C++/Button_LED/Button_pattern.ino
@@ -1,25 +1,35 @@
 int buttonPin1 = 7;
 int buttonPin2 = 8;
-int LED[4] = {13,12,11,10}
+int LED[4] = {13,12,11,10};
 
 void setup()
 {
 pinMode(buttonPin1, INPUT);
 pinMode(buttonPin2, INPUT);
-for( int cnt=0 ; cnt<4; cnt++){
+for( int cnt=0 ; cnt<4; cnt++)
+{
     pinMode (LED[cnt], OUTPUT);  
   }
 }
 
 void loop()
 {
-if digitalRead(buttonPin1==HIGH)
+if (digitalRead(buttonPin1)==HIGH)
 {
 for( int cnt=0 ; cnt<4; cnt++)
 {
-digitalWrite(LED[cnt],HIGH)
+digitalWrite(LED[cnt],HIGH);
 delay(1000);
-digitalWrite(LED[cnt],LOW)
+digitalWrite(LED[cnt],LOW);
+}
+}
+else if (digitalRead(buttonPin2)==HIGH)
+{
+for (int cnt=3; cnt>=0; cnt--)
+{
+  digitalWrite(LED[cnt],HIGH);
+  delay(1000);
+  digitalWrite(LED[cnt],LOW);
 }
 }
 }
