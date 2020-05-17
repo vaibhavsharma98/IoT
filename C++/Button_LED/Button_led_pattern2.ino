4f90189a91acf043a76ed4c21154bb7666a563c7
Vaibhav Sharma <61656345+vaibhavsharma98@users.noreply.github.com>
2020-05-17 11:37:51 +0530

    Update Button_led_pattern2.ino

--- C++/Button_LED/Button_led_pattern2.ino
+++ C++/Button_LED/Button_led_pattern2.ino
@@ -9,16 +9,22 @@ void setup() {
 int count = 0; 
 void loop() {
   // inc button
-  if (digitalRead(BUT1) == HIGH)  {
+  if (digitalRead(buttonPin1) == HIGH)  {
     if (count<4) count++; 
-    while(digitalRead(BUT1) == HIGH); 
+    while(digitalRead(butttonPin1) == HIGH); 
   }
   //dec
-   if (digitalRead(BUT2) == HIGH)  {
+   if (digitalRead(buttonPin2) == HIGH)  {
     if (count>0)count--; 
-    while(digitalRead(BUT2) == HIGH); 
+    while(digitalRead(buttonPin2) == HIGH); 
   }
-
-  
+ 
+  // switch on only required LEDs
+   for(int i=0 ; i<count; i++) digitalWrite(LED[i],HIGH);
+     // switch off rest of  LEDs
+     for(int i= count ; i<4; i++) digitalWrite(LED[i],LOW);
    
 }
+  
+   
+
